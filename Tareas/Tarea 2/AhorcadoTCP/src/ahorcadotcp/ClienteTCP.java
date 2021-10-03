9eb74da457a829f9b6b9cd81193159dac5725393
Grover101 <limachigrover09@gmail.com>
2021-10-03 10:00:20 -0400

    add: Proyecto inical de Ahorcado

---
+++ Tareas/Tarea 2/AhorcadoTCP/src/ahorcadotcp/ClienteTCP.java
@@ -0,0 +1,36 @@
+package ahorcadotcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class ClienteTCP {
+
+    public static void main(String[] args) {
+        int port = 5010;
+        Scanner sc=new Scanner(System.in); 
+        try {
+            String cadena;
+            do {
+                Socket client = new Socket("localhost", port); 
+                PrintStream toServer = new PrintStream(client.getOutputStream());
+                BufferedReader fromServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                System.out.println("Introduzca un cadena: ");               
+                cadena = sc.nextLine();
+                toServer.println(cadena);
+                if (cadena.equals("fin")) break;
+                String result = fromServer.readLine();
+                System.out.println("Cadena Invertida es: " + result);
+            } while (true);
+            
+            
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
---
+++ Tareas/Tarea 2/AhorcadoTCP/src/ahorcadotcp/ServidorTCP.java
@@ -0,0 +1,47 @@
+package ahorcadotcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ServidorTCP {
+
+    public static void main(String[] args) {
+        int port = 5010;
+
+        try {
+            ServerSocket server = new ServerSocket(port);
+            System.out.println("Se inicio el servidor con éxito");
+
+            Socket client;
+            PrintStream toClient;
+            while (true) {
+                client = server.accept(); //conexion
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream())); // el lector
+                System.out.println("Cliente se conecto");
+                String cadena = fromClient.readLine();
+                System.out.println(cadena);
+                if (cadena.equals("fin")) {
+                    System.err.println("SERVIDOR TERMINO LA EJECUCION");
+                    break;
+                }
+                toClient = new PrintStream(client.getOutputStream());
+                toClient.println(Invertir(cadena));
+                System.out.println("Cliente se conecto");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static String Invertir(String cadena) {
+        String cadenaInvertida = "";
+        for (int i = cadena.length() - 1; i >= 0; i--)
+            cadenaInvertida += cadena.charAt(i);
+        return cadenaInvertida;
+    }
+}
