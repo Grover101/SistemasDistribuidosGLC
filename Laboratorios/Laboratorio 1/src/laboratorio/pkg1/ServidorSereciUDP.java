ec3a0cf75b6499f1c4e67c62845b0c7c0bde29e1
Grover101 <limachigrover09@gmail.com>
2021-10-11 21:42:21 -0400

    add: Servidor SereciUDP implementado

--- Laboratorios/Laboratorio 1/src/laboratorio/pkg1/ServidorSereciUDP.java
+++ Laboratorios/Laboratorio 1/src/laboratorio/pkg1/ServidorSereciUDP.java
@@ -21,31 +21,30 @@ public class ServidorSereciUDP {
         try {
 
             DatagramSocket socketUDP = new DatagramSocket(port);
+            System.out.println("Se inicio el servidor Serecil UDP con exito");
             byte[] bufer = new byte[1000];
 
-            while (true) {
-                // Construimos el DatagramPacket para recibir peticiones
-                DatagramPacket peticion
-                        = new DatagramPacket(bufer, bufer.length);
+            // while (true) {
+            // Construimos el DatagramPacket para recibir peticiones
+            DatagramPacket peticion = new DatagramPacket(bufer, bufer.length);
 
-                // Leemos una petición del DatagramSocket
-                socketUDP.receive(peticion);
-                
-                String as = new String(peticion.getData(), 0, peticion.getLength());
+            // Leemos una petición del DatagramSocket
+            socketUDP.receive(peticion);
 
-                String fac = String.valueOf(factorial(Integer.valueOf(as)));
-                
-                byte[] calulo = fac.getBytes();
-                
-                // Construimos el DatagramPacket para enviar la respuesta
-                DatagramPacket respuesta
-                        = new DatagramPacket(calulo, fac.length(),
-                                peticion.getAddress(), peticion.getPort());
+            String datos = new String(peticion.getData(), 0, peticion.getLength());
 
-                // Enviamos la respuesta, que es un eco
-                socketUDP.send(respuesta);
-            }
+            String verificado = Verificar(datos);
 
+            byte[] respuestaConvert = verificado.getBytes();
+
+            // Construimos el DatagramPacket para enviar la respuesta
+            DatagramPacket respuesta = new DatagramPacket(respuestaConvert, verificado.length(), peticion.getAddress(),
+                    peticion.getPort());
+
+            // Enviamos la respuesta, que es un eco
+            socketUDP.send(respuesta);
+            // }
+            socketUDP.close();
         } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
         } catch (IOException e) {
@@ -53,11 +52,10 @@ public class ServidorSereciUDP {
         }
     }
 
-    public static int factorial(int n) {
-        int fact = 1;
-        for (int i = 1; i <= n; i++) {
-            fact *= i;
-        }
-        return fact;
+    public static String Verificar(String cadena) {
+        String[] valores = cadena.split(":");
+        String[] datos = valores[1].split(",");
+        return (datos[0].equals("Walter Jhamil") && datos[1].equals("Segovia Arellano")
+                && datos[2].equals("11-02-1996")) ? "Verificacion Correcta" : "Error fecha nacimiento no correcta";
     }
 }
