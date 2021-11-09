dbc82ac51efa937c09bfc426339ab0a04191a0e7
Grover101 <limachigrover09@gmail.com>
2021-11-08 22:21:35 -0400

    Tarea 6

---
+++ Tareas/Tarea 6/consumoJava/src/consumojava/ConsumoJava.java
@@ -0,0 +1,16 @@
+package consumojava;
+
+/**
+ *
+ * @author HP
+ */
+public class ConsumoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
