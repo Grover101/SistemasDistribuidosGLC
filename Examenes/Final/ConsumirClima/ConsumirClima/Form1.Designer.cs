ddebb35d0c7cc7bca91a9ebe82340a32033babb1
Grover101 <limachigrover09@gmail.com>
2022-01-28 17:32:25 -0400

    add: Cliente de Clima

---
+++ Examenes/Final/ConsumirClima/ConsumirClima/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ConsumirClima
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTemperatura = new System.Windows.Forms.Button();
+            this.btnPorcentaje = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFecha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Victor Mono Oblique", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consulta del Clima";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Resultado:";
+            // 
+            // btnTemperatura
+            // 
+            this.btnTemperatura.Location = new System.Drawing.Point(272, 152);
+            this.btnTemperatura.Name = "btnTemperatura";
+            this.btnTemperatura.Size = new System.Drawing.Size(75, 23);
+            this.btnTemperatura.TabIndex = 2;
+            this.btnTemperatura.Text = "Temperatura";
+            this.btnTemperatura.UseVisualStyleBackColor = true;
+            this.btnTemperatura.Click += new System.EventHandler(this.btnTemperatura_Click);
+            // 
+            // btnPorcentaje
+            // 
+            this.btnPorcentaje.Location = new System.Drawing.Point(437, 152);
+            this.btnPorcentaje.Name = "btnPorcentaje";
+            this.btnPorcentaje.Size = new System.Drawing.Size(75, 23);
+            this.btnPorcentaje.TabIndex = 3;
+            this.btnPorcentaje.Text = "Porcentaje";
+            this.btnPorcentaje.UseVisualStyleBackColor = true;
+            this.btnPorcentaje.Click += new System.EventHandler(this.btnPorcentaje_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Font = new System.Drawing.Font("Victor Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.Location = new System.Drawing.Point(325, 234);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(0, 13);
+            this.resultado.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fecha:";
+            // 
+            // textFecha
+            // 
+            this.textFecha.Location = new System.Drawing.Point(339, 102);
+            this.textFecha.Name = "textFecha";
+            this.textFecha.Size = new System.Drawing.Size(164, 20);
+            this.textFecha.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.btnPorcentaje);
+            this.Controls.Add(this.btnTemperatura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTemperatura;
+        private System.Windows.Forms.Button btnPorcentaje;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFecha;
+    }
+}
+
---
+++ Examenes/Final/ConsumirClima/ConsumirClima/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsumirClima
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTemperatura_Click(object sender, EventArgs e)
+        {
+            String fecha = textFecha.Text;
+            servicio.wsClimaClient ws = new servicio.wsClimaClient();
+            resultado.Text = ws.Temperatura("t", fecha);
+        }
+
+        private void btnPorcentaje_Click(object sender, EventArgs e)
+        {
+            String fecha = textFecha.Text;
+            servicio.wsClimaClient ws = new servicio.wsClimaClient();
+            resultado.Text = ws.Temperatura("p", fecha);
+        }
+    }
+}
